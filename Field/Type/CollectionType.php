4228e2f46ccb8194a7756fffa734a90b0e0a0146
David Coudrier <david.coudrier@gmail.com>
2015-06-12 01:57:37 +0200

    Fix CollectionType fix doc

--- Field/Type/CollectionType.php
+++ Field/Type/CollectionType.php
@@ -1,8 +1,18 @@
 <?php
+/*
+ * This file is part of TableBundle.
+ *
+ * (c) David Coudrier <david.coudrier@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Nours\TableBundle\Field\Type;
 
 use Nours\TableBundle\Field\AbstractFieldType;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class CollectionType
@@ -17,5 +27,18 @@ class CollectionType extends AbstractFieldType
     public function getName()
     {
         return 'collection';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        // Fixes the property_path to omit the association extension
+        $resolver->setDefaults(array(
+            'property_path' => function(Options $options) {
+                return $options['property'];
+            }
+        ));
     }
 }
